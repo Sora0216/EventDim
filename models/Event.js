9bd5b9a5247178ec200730162d071704ec202e68
Mr-E-ricH <158533024+Mr-E-ricH@users.noreply.github.com>
2024-07-29 18:48:15 -0500

    Add Mongoose schemas for Event, Photo, User, and Review models
    - Created Event schema to manage event details and references to photos and reviews
    - Created Photo schema to manage photos associated with events
    - Created User schema to manage user information and references to events and reviews
    - Created Review schema to manage reviews associated with events

--- models/Event.js
+++ models/Event.js
@@ -5,8 +5,9 @@ const EventSchema = new Schema({
   name: { type: String, required: true },
   date: { type: Date, required: true },
   location: { type: String, required: true },
-  menuItems: [{ type: Schema.Types.ObjectId, ref: 'MenuItem' }],
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User' }
+  photos: [{ type: Schema.Types.ObjectId, ref: 'Photo' }],
+  createdBy: { type: Schema.Types.ObjectId, ref: 'User' },
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
 });
 
 module.exports = mongoose.model('Event', EventSchema);
